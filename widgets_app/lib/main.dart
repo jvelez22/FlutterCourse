9ba1ce5b8787be60b27965dc47df9601ece5a633
Juan Carlos Velez <juancarlosvelez@Juans-Mac-mini.local>
2020-06-22 10:56:47 -0500

    Create dynamics routes with onGenerateRoute in main.dart

--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:widgets_app/src/pages/alert_page.dart';
 import 'package:widgets_app/src/pages/avatar_page.dart';
 import 'package:widgets_app/src/pages/home_page.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,8 +13,11 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
         '/': (BuildContext contex) => HomePage(),
-        'alert' : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage()
+        'alert': (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage()
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(builder: (BuildContext contex) => AlertPage());
       },
     );
   }
