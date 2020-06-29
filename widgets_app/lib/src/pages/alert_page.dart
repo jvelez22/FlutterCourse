b4102871e8ba87ca416a90cd85881eb190760f2f
Juan Carlos Velez <juancarlosvelez@Juans-Mac-mini.local>
2020-06-29 09:24:04 -0500

    Alert dialog

--- widgets_app/lib/src/pages/alert_page.dart
+++ widgets_app/lib/src/pages/alert_page.dart
@@ -9,10 +9,48 @@ class AlertPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Alert Page'),
         ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () => _mostrarAlerta(context),
+            child: Text('Mostrar Alerta'),
+            color: Colors.blue,
+            textColor: Colors.white,
+            shape: StadiumBorder(),
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
             child: Icon(Icons.keyboard_return),
             onPressed: () {
               Navigator.pop(context);
             }));
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido...'),
+              FlutterLogo(
+                size: 100.0,
+              )
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Cancelar')),
+            FlatButton(onPressed: () {}, child: Text('Aceptar'))
+          ],
+        );
+      },
+    );
   }
 }
