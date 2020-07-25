08aba224864b93009b6e2dd4f751cec3a7afd9f9
Juan Carlos Velez <juancarlosvelez@Juans-Mac-mini.local>
2020-07-25 09:32:32 -0500

    Checkbox and Switch

--- widgets_app/lib/src/pages/slider_page.dart
+++ widgets_app/lib/src/pages/slider_page.dart
@@ -9,6 +9,8 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _sliderValue = 100.0;
+  bool _blockCheck = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +18,14 @@ class _SliderPageState extends State<SliderPage> {
         title: Text('Slider'),
       ),
       body: Container(
-          padding: EdgeInsets.only(top: 50.0),
+          padding: EdgeInsets.only(top: 20.0),
           child: Column(
-            children: <Widget>[_crearSlider(), Expanded(child: _crearImagen())],
+            children: <Widget>[
+              _crearSlider(),
+              _crearCheckBox(),
+              _crearSwitch(),
+              Expanded(child: _crearImagen())
+            ],
           )),
     );
   }
@@ -27,13 +34,15 @@ class _SliderPageState extends State<SliderPage> {
     return Slider(
         value: _sliderValue,
         min: 10.0,
-        max: 250.0,
+        max: 230.0,
         label: '$_sliderValue',
-        onChanged: (value) {
-          setState(() {
-            _sliderValue = value;
-          });
-        });
+        onChanged: _blockCheck
+            ? null
+            : (value) {
+                setState(() {
+                  _sliderValue = value;
+                });
+              });
   }
 
   Widget _crearImagen() {
@@ -42,5 +51,35 @@ class _SliderPageState extends State<SliderPage> {
             'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/a/ab/Iron_Man_Mark_LXXXV.png/revision/latest?cb=20190405192851&path-prefix=es'),
         width: _sliderValue,
         fit: BoxFit.contain);
+  }
+
+  Widget _crearCheckBox() {
+    // return Checkbox(
+    //     value: _blockCheck,
+    //     onChanged: (isSelected) {
+    //       setState(() {
+    //         _blockCheck = isSelected;
+    //       });
+    //     });
+
+    return CheckboxListTile(
+        title: Text('Block slider'),
+        value: _blockCheck,
+        onChanged: (isSelected) {
+          setState(() {
+            _blockCheck = isSelected;
+          });
+        });
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+        title: Text('Block slider'),
+        value: _blockCheck,
+        onChanged: (isSelected) {
+          setState(() {
+            _blockCheck = isSelected;
+          });
+        });
   }
 }
