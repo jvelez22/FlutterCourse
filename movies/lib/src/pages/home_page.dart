7d1d8a6305b6b8356b7d743d465f4aacd749f12c
Juan Carlos Velez <juancarlosvelez@Juans-Mac-mini.local>
2020-07-27 06:24:36 -0500

    Swiper library added and implemented.

--- movies/lib/src/pages/home_page.dart
+++ movies/lib/src/pages/home_page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Text('Home page')));
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Peliculas en Cartelera'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {})
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[_cardSwiper()],
+          ),
+        ));
+  }
+
+  Widget _cardSwiper() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+      ),
+    );
   }
 }
