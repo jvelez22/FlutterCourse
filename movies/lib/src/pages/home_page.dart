ac9f74f4fa08a2e94aeef5452f4edb0eeb67630a
Juan Carlos Velez <juancarlosvelez@Juans-Mac-mini.local>
2020-10-07 06:46:48 -0500

    Add popular movies

--- movies/lib/src/pages/home_page.dart
+++ movies/lib/src/pages/home_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:movies/src/providers/movies_provider.dart';
 import 'package:movies/src/widgets/card_swiper_widget.dart';
+import 'package:movies/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
   final moviesProvider = MoviesProvider();
 
   @override
   Widget build(BuildContext context) {
+    moviesProvider.getPopular();
     return Scaffold(
         appBar: AppBar(
           centerTitle: false,
@@ -18,7 +20,8 @@ class HomePage extends StatelessWidget {
         ),
         body: Container(
           child: Column(
-            children: <Widget>[_cardSwiper()],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [_cardSwiper(), _footer(context)],
           ),
         ));
   }
@@ -34,6 +37,33 @@ class HomePage extends StatelessWidget {
               height: 400.0, child: Center(child: CircularProgressIndicator()));
         }
       },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text("Populares",
+                  style: Theme.of(context).textTheme.subtitle1)),
+          SizedBox(height: 5.0),
+          StreamBuilder(
+              stream: moviesProvider.popularsStream,
+              builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+                if (snapshot.hasData) {
+                  return MovieHorizontal(
+                      movies: snapshot.data,
+                      nextPage: moviesProvider.getPopular);
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              })
+        ],
+      ),
     );
   }
 }
--- movies/lib/src/providers/movies_provider.dart
+++ movies/lib/src/providers/movies_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart';
@@ -7,15 +8,49 @@ class MoviesProvider {
   String _apikey = '4543fb4e80c484b01b668b6cfd4f23f6';
   String _url = 'api.themoviedb.org';
   String _language = 'es-MEX';
+  int _popularsPage = 0;
+  bool isLoading = false;
+  List<Movie> _populars = List();
+
+  final _popularsStreamController = StreamController<List<Movie>>.broadcast();
+
+  Function(List<Movie>) get popularsSink => _popularsStreamController.sink.add;
+  Stream<List<Movie>> get popularsStream => _popularsStreamController.stream;
 
   Future<List<Movie>> getNowPlaying() async {
     final url = Uri.https(_url, '3/movie/now_playing',
         {'api_key': _apikey, 'language': _language});
 
+    return await _processResponse(url);
+  }
+
+  void getPopular() async {
+    if (isLoading) {
+      return;
+    }
+    isLoading = true;
+    _popularsPage++;
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key': _apikey,
+      'language': _language,
+      'page': _popularsPage.toString()
+    });
+
+    final response = await _processResponse(url);
+    _populars.addAll(response);
+    popularsSink(_populars);
+    isLoading = false;
+  }
+
+  Future<List<Movie>> _processResponse(Uri url) async {
     final response = await get(url);
     final decodeData = json.decode(response.body);
     final results = Movies.fromJsonList(decodeData['results']);
 
     return results.movies;
+  }
+
+  void dispose() {
+    _popularsStreamController.close();
   }
 }
